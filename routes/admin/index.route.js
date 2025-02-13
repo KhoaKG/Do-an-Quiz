4b22ea13a335c8a62dbe1b144402834b9295ba3d
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-02-13 21:58:09 +0700

    10

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoute = require("./dashboard.route")
 const productsRoute = require("./products.route")
 const productsCategory = require("./products-category.route")
+const rolesRoute = require("./roles.route")
 const systemConfig = require("../../config/system")
 module.exports = (app)=>{
     const DATA_PATH = systemConfig.prefixAdmin
@@ -9,4 +10,6 @@ module.exports = (app)=>{
     app.use(DATA_PATH+ "/products", productsRoute)
 
     app.use(DATA_PATH+ "/products-category", productsCategory)
+
+    app.use(DATA_PATH+ "/roles", rolesRoute)
 }
