4fcb228b00ef755d24fcfa66019b4a2b9c118f1c
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-02-13 23:15:03 +0700

    12

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,15 +4,20 @@ const productsCategory = require("./products-category.route")
 const rolesRoute = require("./roles.route")
 const accountsRoute = require("./accounts.route")
 const systemConfig = require("../../config/system")
+const authRoute = require("./auth.route")
+const authMiddleware = require("../../middleware/admin/auth.middleware")
+
 module.exports = (app)=>{
     const DATA_PATH = systemConfig.prefixAdmin
-    app.use(DATA_PATH + "/dashboard", dashboardRoute)
+    app.use(DATA_PATH + "/dashboard", authMiddleware.requireAuth,dashboardRoute)
 
-    app.use(DATA_PATH+ "/products", productsRoute)
+    app.use(DATA_PATH+ "/products", authMiddleware.requireAuth,productsRoute)
 
-    app.use(DATA_PATH+ "/products-category", productsCategory)
+    app.use(DATA_PATH+ "/products-category", authMiddleware.requireAuth,productsCategory)
 
-    app.use(DATA_PATH+ "/roles", rolesRoute)
+    app.use(DATA_PATH+ "/roles", authMiddleware.requireAuth,rolesRoute)
 
-    app.use(DATA_PATH+ "/accounts", accountsRoute)
+    app.use(DATA_PATH+ "/accounts", authMiddleware.requireAuth, accountsRoute)
+
+    app.use(DATA_PATH+ "/auth", authRoute)
 }
